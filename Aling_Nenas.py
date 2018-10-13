347bc8642f1bcca4ac62d3f28b1106d4b88fd1a0
Roweann-Morito <44015578+Roweann-Morito@users.noreply.github.com>
2018-10-13 17:32:56 +0800

    Add files via upload

---
+++ Aling_Nenas.py
@@ -0,0 +1,4 @@
+total_bill = input('How much is your total bill? ')
+payment = input('How much is your payment? ')
+change = int(payment)-int(total_bill)
+print('Hi! Your change is {}'.format(change)) 
