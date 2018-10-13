10ec1183b9e6959bace9097fce34bf4ff4e0b90e
Roweann-Morito <44015578+Roweann-Morito@users.noreply.github.com>
2018-10-13 17:01:52 +0800

    Add files via upload

---
+++ first_python_code.py
@@ -0,0 +1,10 @@
+organization = 'WomenWhoCode!'
+
+print('Hello WomenWhoCode')
+
+print('Hello' + organization)
+
+print('Hello {}'.format(organization))
+
+your_name = 'Rowee'
+print('{} loves Python!'.format(your_name))
